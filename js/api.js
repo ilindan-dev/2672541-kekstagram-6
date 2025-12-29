0696e0fdc7d19b9dab31bf885d1e4d5d6cd4a479
Ilya Gellert <gellertilya@gmail.com>
2025-12-29 10:21:53 +0300

    feat: add photo filtering

--- js/api.js
+++ js/api.js
@@ -9,23 +9,16 @@ const ErrorText = {
   SEND_DATA: 'Не удалось отправить форму. Попробуйте ещё раз',
 };
 
-const getData = (onSuccess, onFail) => {
+const getData = () =>
   fetch(`${BASE_URL}${Route.GET_DATA}`)
     .then((response) => {
       if (!response.ok) {
         throw new Error();
       }
       return response.json();
-    })
-    .then((data) => {
-      onSuccess(data);
-    })
-    .catch(() => {
-      onFail(ErrorText.GET_DATA);
     });
-};
 
-const sendData = (onSuccess, onFail, body) => {
+const sendData = (body, onSuccess, onFail) => {
   fetch(`${BASE_URL}${Route.SEND_DATA}`, {
     method: 'POST',
     body,
